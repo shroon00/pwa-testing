5f672e4c13ec554e19c386f567e72e022e084cb6
shroon00 <shireenalzubaidy02@gmail.com>
2026-01-06 12:54:13 +0300

    2

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
 
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  disable: false,
   register: true,
   skipWaiting: true,
 });
